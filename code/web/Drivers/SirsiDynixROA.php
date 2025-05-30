8e57121d8981f54fa947fd4d0053fc3fe248463e
LeoStoyanovByWater <leo.stoyanov@bywatersolutions.com>
2025-05-30 08:54:56 -0500

    BWS-PKG - DIS-903: Add delay before requesting Symphony to find the newly registered user.

--- code/web/Drivers/SirsiDynixROA.php
+++ code/web/Drivers/SirsiDynixROA.php
@@ -90,13 +90,16 @@ class SirsiDynixROA extends HorizonAPI {
 		}
 	}
 
-	function findNewUser($patronBarcode, $patronUsername) {
+	function findNewUser($patronBarcode, $patronUsername): User|bool {
 		// Creates a new user like patronLogin but looks up user by barcode.
 		// Note: The user pin is not supplied in the Account Info Lookup call.
 		$sessionToken = $this->getStaffSessionToken();
 		if (!empty($sessionToken)) {
 			$webServiceURL = $this->getWebServiceURL();
 			$includeFields = urlEncode("firstName,lastName,privilegeExpiresDate,preferredAddress,preferredName,address1,address2,address3,library,primaryPhone,profile,pin,blockList{owed}");
+			// Symphony needs time to process the full information of the user when registering,
+			// so add a delay to ensure the results array is populated.
+			usleep(500000);
 			$lookupMyAccountInfoResponse = $this->getWebServiceResponse('findNewUser', $webServiceURL . '/user/patron/search?q=ID:' . $patronBarcode . '&rw=1&ct=1&includeFields=' . $includeFields, null, $sessionToken);
 			if (!empty($lookupMyAccountInfoResponse->result) && $lookupMyAccountInfoResponse->totalResults == 1) {
 				$userID = $lookupMyAccountInfoResponse->result[0]->key;
